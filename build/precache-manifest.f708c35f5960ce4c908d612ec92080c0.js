05df2a6a1e42ac8d587838c246b48bf3bb73b411
sabbar <habibkharkovi181@gmail.com>
2020-04-13 17:26:25 -0700

    build

---
+++ build/precache-manifest.f708c35f5960ce4c908d612ec92080c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8601e1b2d22543537a1b9b279f672bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa73394f225ec3ab85f1",
+    "url": "/static/css/main.15fc4f7b.chunk.css"
+  },
+  {
+    "revision": "be44893f6574e938b93b",
+    "url": "/static/js/2.1e28d504.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.1e28d504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa73394f225ec3ab85f1",
+    "url": "/static/js/main.0fac26c5.chunk.js"
+  },
+  {
+    "revision": "3c5d524c3a8a749f45a0",
+    "url": "/static/js/runtime-main.53d8960f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
